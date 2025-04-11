88590bb91d09ed3524351e087eaacf78d4ebf06d
Lemon1151 <smile1151@foxmail.com>
2025-04-11 11:30:04 +0800

    Add Ubuntu 25.04 (Plucky Puffin) suite

---
+++ config/suites/plucky.sh
@@ -0,0 +1,10 @@
+# shellcheck shell=bash
+
+export RELASE_NAME="Ubuntu 25.04 (Plucky Puffin)"
+export RELASE_VERSION="25.04"
+
+export KERNEL_REPO="https://github.com/Lemon1151/linux-kernel.git"
+export KERNEL_BRANCH="plucky"
+export KERNEL_FLAVOR="rockchip"
+
+export EXTRA_PPAS="jjriek/rockchip"
